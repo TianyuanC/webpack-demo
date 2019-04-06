a1b0003e64cafd8a0385530304743ee51e6928fb
Ryan Chu <tianyuan.chu@gmail.com>
2019-04-06 01:55:08 -0700

    create-react-app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-    return <div>Hello React!</div>;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 };
 
 ReactDOM.render(<Index />, document.getElementById("root"));
